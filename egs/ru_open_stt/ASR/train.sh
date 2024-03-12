9227cc7b98a3b9ec3e5aefb3bf3f97c21fab6f56
root <root@x79543b7ea90481f8564f2d961b13f2b-task0-0.x79543b7ea90481f8564f2d961b13f2b.dc49c45bdd0b4cb6920e6ac6dbab2a13.svc.cluster.local>
2024-03-12 17:05:23 +0800

    adapt to cloudbrain

--- egs/ru_open_stt/ASR/train.sh
+++ egs/ru_open_stt/ASR/train.sh
@@ -1,11 +1,9 @@
-export CUDA_VISIBLE_DEVICES="1"
-
 ./zipformer/train.py \
-  --world-size 1 \
+  --world-size 8 \
   --use-fp16 1 \
   --causal 1 \
   --exp-dir zipformer/exp_lr_epoch_3.5_fp16 \
   --bpe-model data/lang_bpe_5000/bpe.model \
   --keep-last-k 1 \
-  --max-duration 3000 \
+  --max-duration 300 \
   --num-workers 20
