33c9e86f541a6d46763556659ba35e03bdec0925
yangb <yangb05@pcl.ac.cn>
2024-05-09 15:05:25 +0800

    train online

--- egs/arabic/SADA_ASR/train_online.sh
+++ egs/arabic/SADA_ASR/train_online.sh
@@ -1,13 +1,14 @@
-export CUDA_VISIBLE_DEVICES="1,2,3"
+export CUDA_VISIBLE_DEVICES="0"
 
 ./zipformer/train.py \
-  --world-size 3 \
+  --world-size 1 \
   --use-fp16 1 \
   --causal 1 \
   --exp-dir zipformer/exp_online \
-  --bpe-model data/lang_bpe_10000/bpe.model \
+  --bpe-model data/lang_bpe_500/bpe.model \
   --keep-last-k 1 \
   --max-duration 1500 \
-  --num-workers 20
+  --num-workers 20 \
+  --master-port 12355
   
   
